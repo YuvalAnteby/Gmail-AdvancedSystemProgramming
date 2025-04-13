7775a423502060a8374ff056bf431f49bbba6442
yuval <yuvala2001@gmail.com>
2025-04-13 13:26:41 +0300

    Added implemention of FilePersistence class

--- src/data_persistence/FilePersistence.cpp
+++ src/data_persistence/FilePersistence.cpp
@@ -1,4 +1,80 @@
 // Author(s): Yuval Anteby
 #include "FilePersistence.h"
 #include <fstream>
+#include <iostream>
+
+/**
+ * Constructor
+ */
+FilePersistence::FilePersistence() {
+    
+}
+
+std::vector<std::vector<bool>> FilePersistence::loadBitArrays() {
+    std::ifstream file(bitArrayPath.c_str());
+    // If the file doesn't exist, create it and return an empty file.
+    if (!file.is_open()) {
+        std::ofstream createFile(bitArrayPath.c_str());
+        return {};
+    }
+    // Read from the file and convert to the correct variable type (from a string)
+    std::vector<std::vector<bool>> loadedBitsArrays;
+    std::string line;
+    while(std::getline(file, line)) {
+        std::vector<bool> bitsArray;
+        // Convert each character in the line to a boolean value representing 1,0
+        for (char c : line) {
+            bitsArray.push_back(c == '1');
+        }
+        loadedBitsArrays.push_back(bitsArray);
+    }
+    file.close();
+    return loadedBitsArrays;
+}
+
+void FilePersistence::appendBitArray(const std::vector<bool>& bits) {
+    // If the input is empty, throw an exception - invalid argument
+    if (bits.empty()) {
+        throw std::invalid_argument("Can't add an empty bit array");
+    }
+
+    std::ofstream file(bitArrayPath.c_str(), std::ios::app);
+    // Convert each bool to 1/0 chars and add to the file
+    for (bool bit: bits) {
+        file << (bit ? '1' : '0');
+    }
+    // End the line (new bit array)
+    file << '\n';
+    file.close();
+}
+
+std::vector<std::string> FilePersistence::loadBlacklist() {
+    std::ifstream file(blacklistPath.c_str());
+    // If the file doesn't exist, create it and return an empty file.
+    if (!file.is_open()) {
+        std::ofstream createFile(blacklistPath.c_str());
+        return {};
+    }
+    // Read from the file
+    std::vector<std::string> loadedUrls;
+    std::string line;
+    while (std::getline(file, line)) {
+        loadedUrls.push_back(line);
+    }
+    file.close();
+    return loadedUrls;
+}
+
+void FilePersistence::appendBlacklistedUrl(const std::string& url) {
+    // If the input is empty, throw an exception - invalid argument
+    if (url.empty()) {
+        throw std::invalid_argument("Can't add an empty string URL");
+    }
+
+    std::ofstream file(blacklistPath.c_str(), std::ios::app);
+    file << url;
+    // End the line (new URL)
+    file << '\n';
+    file.close();
+}
 
