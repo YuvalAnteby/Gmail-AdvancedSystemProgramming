7c34f036c84119194c71a9555cc304d3738b9a6b
dor-darmon <161502302+dor-darmon@users.noreply.github.com>
2025-04-22 20:14:06 +0300

    as

--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,151 @@
-
-#include "BloomFilter.h"
 #include "data_persistence/FilePersistence.h"
 #include <iostream>
 #include <sstream>
 #include <sstream>
 #include <iostream>
 #include <cctype>
+#include <algorithm>
+#include <regex>
 
-int main()
-{
-    size_t size;
-    int numHashes;
+/**
+ * Processes a line of input.
+ * @param line line of the user's input
+ * @return true if valid numbers were found and no invalid characters existed between them.
+ * Otherwise, returns false, prints "FALSE"
+ */
+bool processLine(const std::string& line) {
+    std::istringstream iss(line);
+    std::string token;
+    bool hasDigits = false;
+    std::vector<int> intsResult;
+    while (iss >> token) {
+        // Skip entire line if it starts with letters
+        if (!hasDigits && std::any_of(token.begin(), token.end(), ::isalpha))
+            return {};
 
-    // Receive the initial configuration (array size and number of hash functions).
-    std::string firstLine;
-    std::getline(std::cin, firstLine);
-    std::istringstream configStream(firstLine);
-    configStream >> size >> numHashes;
-
-    // Check if the number of hashes is valid (should be 1 or 2)
-    if (numHashes != 1 && numHashes != 2)
-    {
-        std::cout << "false" << std::endl;
-        return 0;
-    }
-
-    // Create an instance of the FilePersistence class
-    FilePersistence persistence;
-
-    // Create a BloomFilter instance with the given size and number of hash functions
-    BloomFilter filter(size, numHashes, &persistence);
-
-    // Load any existing data from the file system into the Bloom filter
-    filter.loadExistingData();
-
-    std::string line;
-    // Continuously read lines from standard input until EOF or an invalid line is encountered
-    while (true)
-    {
-        std::getline(std::cin, line);
-
-        // If the line contains a new configuration (size and number of hash functions), update the configuration.
-        std::istringstream newConfigStream(line);
-        size_t newSize;
-        int newNumHashes;
-        if (newConfigStream >> newSize >> newNumHashes)
-        {
-            // Ensure that the number of hash functions is valid (1 or 2)
-            if (newNumHashes != 1 && newNumHashes != 2)
-            {
-                std::cout << "false" << std::endl;
-                continue;
-            }
-            // Update the configuration with the new size and number of hash functions
-            filter.updateConfig(newSize, newNumHashes);
-            continue;
-        }
-        // Ensure the line is not empty and contains only digits
-        if (
-            !line.empty() && [](const std::string &l)
-            {
-                // Check if all characters in the line are digits
-                for (char c : l)
-                {
-                    if (!isdigit(c))
-                        return false; 
-                }
-
-                std::istringstream iss(l); 
-                std::string token;
-                bool first = true;
-
-                // Iterate through each token in the string
-                while (iss >> token)
-                {
-                    // After the first token, it must be '1' or '2'
-                    if (!first && token != "1" && token != "2")
-                        return false;
-
-                    first = false;
-                }
-
-                // Return true only if there was at least one token after the first
-                return !first; 
-            }(line) 
-        )
-        {
-            // Extract the URL , skip the first 2 characters, which are the number and spac.
-            std::string url = line.substr(2);
-
-            /// TODO: Continue writing them in a folder. (yuval)
-            // If the input starts with '1', add the URL to the filter
-            if (line[0] == '1')
-            {
-                filter.add(url);
-            }
-            /// TODO: Change the second according to the rules in the exercise.(ROI)
-            // If the input starts with '2', check if the URL might be blacklisted
-            else if (line[0] == '2')
-            {
-                bool maybe = filter.mightContain(url);
-                if (!maybe)
-                {
-                    std::cout << "false" << std::endl;
-                }
-                else
-                {
-                    // Verify if the URL is actually blacklisted
-                    bool really = filter.isReallyBlacklisted(url);
-                    std::cout << "true " << (really ? "true" : "false") << std::endl;
-                }
-            }
-        }
-        else
-        {
+        // If token is not all digits, it's invalid
+        if (!std::all_of(token.begin(), token.end(), ::isdigit)) {
             std::cout << "false" << std::endl;
-            continue;
+            return {};
         }
 
-        return 0;
+        hasDigits = true;
     }
+
+    // If numbers were found, print newline and return true
+    if (hasDigits) {
+        std::cout << std::endl;
+        return true;
+    }
+
+    // No numbers found, line is ignored
+    return {};
+}
+/**
+ * Check if the URL is valid using a basic regex pattern.
+ * @param url string of a URL to be checked
+ * @return true if the URL is of a valid regex
+ */
+bool isValidURL(const std::string& url) {
+    const std::regex pattern(R"(^(http|https):\/\/[a-zA-Z0-9\-\.]+\.[a-z]{2,}([\/\w\.-])\/?$)");
+    return std::regex_match(url, pattern);
+}
+
+/**
+ * Check if all characters in the string are digits or whitespace.
+ * @param line string of the user's choice of command & the url string
+ * @return false if there is no alphabetic char or integer char in the current char of the string
+ */
+bool containsOnlyDigitsAndWhitespace(const std::string& line) {
+    for (char c : line) {
+        if (!std::isdigit(c) && !std::isspace(c))
+            return false;
+    }
+    return true;
+}
+
+/**
+ * Validate command structure.
+ * The first token is ignored, all following tokens must be "1" or "2".
+ * @param line string of the user's choice of command & the url string
+ * @return true if the choice of the valid options (for now 1 or 2), otherwise false
+ */
+bool hasValidCommandStructure(const std::string& line) {
+    std::istringstream iss(line);
+    std::string token;
+    bool first = true;
+
+    while (iss >> token) {
+        if (!first && token != "1" && token != "2")
+            return false;
+        first = false;
+    }
+
+    return !first;
+}
+
+/**
+ * Validate line before processing:
+ * - Not empty
+ * - Contains only digits/whitespace
+ * - Valid command structure
+ * @param line string of the user's choice of command & the url string
+ * @return true if the line is valid (has choice & valid URL), otherwise false
+ */
+bool isValidLine(const std::string& line) {
+    return !line.empty() &&
+           containsOnlyDigitsAndWhitespace(line) &&
+           hasValidCommandStructure(line);
+}
+
+/**
+ * Process a valid command line:
+ * - If it starts with '1', add the URL
+ * - If it starts with '2', check against the blacklist
+ * @param line string of the user's choice of command & the url string
+ * @param filter bloom filter object reference
+ */
+void handleUserChoice(const std::string& line) {
+    if (line.size() < 3) {
+        std::cout << "false" << std::endl;
+        return;
+    }
+
+    std::string url = line.substr(2);  // Extract URL after command and space
+
+    if (!isValidURL(url)) {
+        std::cout << "false" << std::endl;
+        return;
+    }
+
+    if (line[0] == '1') {
+        //filter.add(url);
+        // TODO: insert new URL to filter here
+    } else if (line[0] == '2') {
+        // TODO: Roee's work here
+    }
+}
+
+/**
+ * Main loop that reads and processes input lines.
+ */
+int main() {
+    // TODO: use here bloom filter object by Roee
+    //BloomFilter filter;
+    std::string line;
+    bool firstLineFlag = true;
+    while (std::getline(std::cin, line)) {
+        // Skip to next line if the first line was invalid
+        if (firstLineFlag && !processLine(line)) {
+            continue;
+        }
+        firstLineFlag = false;
+        if (isValidLine(line)) {
+            handleUserChoice(line);
+        } else {
+            std::cout << "false" << std::endl;
+        }
+    }
+
+    return 0;
 }
