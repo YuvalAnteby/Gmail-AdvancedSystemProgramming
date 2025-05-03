3a1dba2dd452bc07950919e5aaecdff89312d447
yuval <yuvala2001@gmail.com>
2025-05-03 20:59:29 +0300

    Created enum for commands

---
+++ server_cpp/src/utils/BloomFilterCommandEnum.h
@@ -0,0 +1,14 @@
+// Author(s): Yuval Anteby
+#ifndef BLOOM_FILTER_COMMAND_ENUM_H
+#define BLOOM_FILTER_COMMAND_ENUM_H
+
+/**
+* Enum to use for bloom filter commands, values set per the assignment instructions as needed, the rest by us.
+*/
+enum BloomFilterCommandEnum {
+    INSERT = 1,
+    CHECK = 2,
+    DELETE = 3 // value made up by Yuval, change if needed in future assignment
+};
+
+#endif
