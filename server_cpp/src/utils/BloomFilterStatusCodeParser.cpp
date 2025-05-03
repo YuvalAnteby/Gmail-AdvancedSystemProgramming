322078d6a2a3d8e2de02b984d9ba27c97441b08d
yuval <yuvala2001@gmail.com>
2025-05-03 20:58:57 +0300

    Created status code enum for results, including a parser to messages

---
+++ server_cpp/src/utils/BloomFilterStatusCodeParser.cpp
@@ -0,0 +1,27 @@
+// Author(s): Yuval Anteby
+
+#include <string>
+#include <utils/BloomFilterStatusEnum.h>
+
+/**
+ * Convert the bloom filter status code to a message
+ * @param statusCode enum of the status code
+ * @return a full message containing the code & message.
+ * e.g. "404 Not Found"
+ */
+std::string toStatusMessage(const BloomFilterStatusEnum statusCode) {
+    switch (statusCode) {
+        case OK:
+            return "200 Ok";
+        case CREATED:
+            return "201 Created";
+        case NO_CONTENT:
+            return "204 No Content";
+        case BAD_REQUEST:
+            return "400 Bad Request";
+        case NOT_FOUND:
+            return "404 Not Found";
+        default:
+            return "";
+    }
+}
---
+++ server_cpp/src/utils/BloomFilterStatusEnum.h
@@ -0,0 +1,16 @@
+// Author(s): Yuval Anteby
+#ifndef BLOOM_FILTER_STATUS_ENUM_H
+#define BLOOM_FILTER_STATUS_ENUM_H
+
+/**
+* Enum to use for status codes, values set per the assigment instructions.
+*/
+enum BloomFilterStatusEnum {
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404
+};
+
+#endif
