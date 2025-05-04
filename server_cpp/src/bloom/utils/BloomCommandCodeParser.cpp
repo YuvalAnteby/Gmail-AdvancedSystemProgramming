311694c0ab0b3b06da1ed896e5c72e7e742701df
yuval <yuvala2001@gmail.com>
2025-05-04 18:19:23 +0300

    Added in the command parser support for use of 1,2 prefix of commands from Ex1

--- server_cpp/src/bloom/utils/BloomCommandCodeParser.cpp
+++ server_cpp/src/bloom/utils/BloomCommandCodeParser.cpp
@@ -28,10 +28,10 @@ std::string toCommandString(const BloomFilterCommandEnum commandCode) {
  * @return enum of the command code. e.g. POST (=1)
  */
 BloomFilterCommandEnum toCommandCode(const std::string &commandString) {
-    if (commandString == "POST") {
+    if (commandString == "POST" || commandString == "1") {
         return POST;
     }
-    if (commandString == "GET") {
+    if (commandString == "GET" || commandString == "2") {
         return GET;
     }
     if (commandString == "DELETE") {
