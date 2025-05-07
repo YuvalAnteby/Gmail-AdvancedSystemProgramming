18ab46970deb5c911e1267bef6edda896a43075b
yuval <yuvala2001@gmail.com>
2025-05-07 14:51:29 +0300

    GA-44 created class to set up the TCP server and accept new connections

---
+++ server_cpp/src/network/TCPSocketServer.h
@@ -0,0 +1,28 @@
+// @Author(s): Yuval Anteby
+#ifndef TCP_SOCKET_SERVER_H
+#define TCP_SOCKET_SERVER_H
+
+
+class TCPSocketServer {
+    const int m_serverPort;
+    const int m_maxClients;
+    int m_serverSocket;
+    int m_clientSocket;
+
+public:
+    // Constructor for server with support for only 1 client
+    TCPSocketServer(int serverPort);
+
+    // Constructor with a dynamic amount of max clients
+    TCPSocketServer(int serverPort, int maxClients);
+
+    // Destructor
+    ~TCPSocketServer();
+
+    void acceptNewClient();
+
+    int getClientSocket() const;
+};
+
+
+#endif
