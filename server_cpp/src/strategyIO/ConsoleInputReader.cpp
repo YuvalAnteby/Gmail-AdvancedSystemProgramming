e19ea7845a6b658b236207d1c861c0de2d4cbcfc
yuval <yuvala2001@gmail.com>
2025-05-07 20:43:56 +0300

    GA-55 fixed the bug

--- server_cpp/src/strategyIO/ConsoleInputReader.cpp
+++ server_cpp/src/strategyIO/ConsoleInputReader.cpp
@@ -18,5 +18,9 @@ std::string ConsoleInputReader::readLine() {
     //TODO add validation tests here
     std::string line;
     std::getline(std::cin, line);
+    // Enables exiting using the shortcut in CLI
+    if (std::cin.eof()) {
+        exit(0);
+    }
     return line;
 }
