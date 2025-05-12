a34d3124aac8d9d6636eee4bc6742c87d37f3e1d
yuval <yuvala2001@gmail.com>
2025-05-12 20:35:24 +0300

    added comments

--- python_client/src/main.py
+++ python_client/src/main.py
@@ -3,14 +3,16 @@ import sys
 import socket
 
 # Run the main class for client
-if len(sys.argv) != 3:
+# We need at least 3 args (initial element, server's ip, port)
+if len(sys.argv) >= 3:
     sys.exit(1)
 # Take the ip and port from command-arg
 ip = sys.argv[1]
 port = int(sys.argv[2])
-
+# Connect using TCP to the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
+# Get the first input to send to the server and loop until disconnecting
 message = input()
 while not message == 'close_client':
     s.send(bytes(message, "utf-8"))
