52988652cf59e7d48a070a5d3f801ad424ce38ee
dor-darmon <dordarmon2000@gmail.com>
2025-05-27 16:15:46 +0300

    fix rout users  and labels

--- web_server/controllers/users.js
+++ web_server/controllers/users.js
@@ -1,9 +1,5 @@
 //# Author: dor darmon 
-const {
-    getUserById,
-    getAllUsers,
-    createUser
-} = require('../models/users')
+const Users = require('../models/users')
 
 /**
  * Signup is to create new user if all required fields are providded
@@ -11,41 +7,60 @@ const {
 const signupUser = (req, res) => {
     const { fullName, mail, password, dateOfBirth, image } = req.body;
 
-    if (!fullName || !mail || !password || !dateOfBirth) {
-        return res.status(400).json({
-            error:
-                'Missing required field'
-        });
+    if (!fullName) {
+        return res.status(400).json({ error: 'Name is required' });
+
+    } else if (!mail) {
+        return res.status(400).json({ error: 'mail is required' });
+
+    } else if (!password) {
+        return res.status(400).json({ error: 'password is required' });
+
+    } else if (!dateOfBirth) {
+        return res.status(400).json({ error: 'date Of Birth is required' });
     }
 
     // check if email already esists
     if (mailExist)
-        return res.status(400).json({error: 'Email already registered'});
+        return res.status(400).json({ error: 'email already esists' });
 
     const newUser = createUser(fullName, mail, password, dateOfBirth, image || null)
     return res.status(201).location('/api/users/${newUser.id}').send();
 }
- 
+
 // check if email already esists
-const mailExist=(mail)=>{
-    mailExist =getAllUsers().find(user=> user.mail === mail)
+const mailExist = (mail) => {
+    mailExist =  Users.getAllUsers().find(user => user.mail === mail)
 }
 
 //Get user by ID
 const getUser = (req, res) => {
     const id = Number(req.params.id);
-    const user = getUserById(id);
+    const user = Users.getUserById(id);
 
     if (!user) {
-        return res.status(404).json({ error: 'Userr not found'});
+        return res.status(404).json({ error: 'Not Found' });
     }
 
     const { password, ...safeUser } = user;
     return res.status(200).json(safeUser);
 }
 
-/// TODO: login using email and password 
+// login using email and password 
 const loginUser = (req, res) => {
+    const { mail, password } = req.body;
 
+    if (!mail || !password) {
+        return res.status(400).json({ error: 'mail and password required' });
+    }
+
+    const user = Users.getAllUsers().find(user => user.mail === mail && user.password === password)
+
+    if (!user) {
+        return res.status(401).json({ error: 'wrong mail or password' });
+    }
+
+    return res.status(200).json({ id: user.id });
 }
+
 module.exports = { signupUser, getUser, loginUser };
