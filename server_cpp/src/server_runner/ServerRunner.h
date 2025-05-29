804b38e8a61395fbd6c7d418571f5476e1b6fd49
roeeHaim <roeechaim@gmail.com>
2025-05-29 12:56:22 +0300

    changed all that yuval has asked me to

--- server_cpp/src/server_runner/ServerRunner.h
+++ server_cpp/src/server_runner/ServerRunner.h
@@ -3,6 +3,16 @@
 #include <mutex>
 #include "../data_persistence/IDataPersistence.h"
 
+/**
+ * Starts the server with the given configuration.
+ *
+ * @param port Port number on which the server should listen.
+ * @param bloomSize Size of the Bloom filter to be used.
+ * @param configInts Additional configuration integers for server setup.
+ * @param dataSource Pointer to the data persistence interface (used to access or store data).
+ * @param dataMutex Pointer to the mutex used for synchronizing access to the data source.
+ * @return 0 on successful execution, or a non-zero error code.
+ */
 int runServer(
         int port,
         int bloomSize,
