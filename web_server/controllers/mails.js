a7649845881f2722c71a2f5a5e124bc597c5da84
yuval <yuvala2001@gmail.com>
2025-05-29 13:55:46 +0300

    GA-80 merge PR #59 GA-80 GET query to main-Ex3

--- web_server/controllers/mails.js
+++ web_server/controllers/mails.js
@@ -18,6 +18,26 @@ const getLastMailsOrdered = (req, res) => {
 }
 
 /**
+ * Finds and returns a mail with the given id.
+ * @param req request
+ * @param res response
+ * @returns a mail object, if there's no id in input returns 400, if there's no mail with the id returns 404.
+ */
+const getMailById = (req, res) => {
+    // Make sure we got an id in the request
+    const id = parseInt(req.params.id);
+    if (isNaN(id))
+        return res.status(400).json({error: 'No valid mail ID was given'});
+
+    // Find the mail with the given id
+    const mail = Mails.getMailById(id);
+    // Return 404 if not found, or a 200 with the mail as a json object
+    if (!mail)
+        return res.status(404).json({error: `No mail found with ID: ${id}`});
+    return res.status(200).json(mail);
+}
+
+/**
  * Creates a new mail message, adds the mail to the relevant users.
  * A user has to be authenticated in order to access their mails.
  * If the mail contains a blacklisted URL, the mail will not be created.
@@ -139,4 +159,4 @@ const getMailsByQuery = (req, res) => {
     return res.status(200).json(mails);
 }
 
-module.exports = {getLastMailsOrdered, createNewMail, editMailById, deleteMailById, getMailsByQuery};
+module.exports = {getLastMailsOrdered, getMailById, createNewMail, editMailById, deleteMailById, getMailsByQuery};
