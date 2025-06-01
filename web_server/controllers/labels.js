06c118504273314834c6d0921c24680f559015ad
yuval <yuvala2001@gmail.com>
2025-06-01 18:35:43 +0300

    Fixed wrong user id fetching in labels

--- web_server/controllers/labels.js
+++ web_server/controllers/labels.js
@@ -26,7 +26,7 @@ const getAllLabels = (req, res) => {
  *   - 400 Bad Request if creation fails
  */
 const createNewLabel = (req, res) => {
-    const userId = parseInt(req.headers['userid'], 10);
+    const userId = Number(req.headers['user-id']);
     const labelName = req.body.name;
 
     if (!labelName) {
@@ -63,7 +63,7 @@ const createNewLabel = (req, res) => {
  * @returns the label object { id, name } pretty-printed, or 404 if not found.
  */
 const getLabelById = (req, res) => {
-    const id = parseInt(req.params.id, 10);
+    const id = Number(req.headers['user-id']);
     const label = Labels.getLabelById(id);
 
     if (!label) {
@@ -96,7 +96,7 @@ const getLabelById = (req, res) => {
  *   - 404 Not Found if label does not exist
  */
 const editLabel = (req, res) => {
-    const id = parseInt(req.params.id, 10);
+    const id = Number(req.headers['user-id']);
     const name = req.body.name;
 
     if (!name) {
@@ -133,7 +133,7 @@ const editLabel = (req, res) => {
  *   - 404 Not Found if label does not exist
  */
 const deleteLabel = (req, res) => {
-    const id = parseInt(req.params.id, 10);
+    const id = Number(req.headers['user-id']);
     const success = Labels.deleteLabel(id);
 
     if (!success) {
