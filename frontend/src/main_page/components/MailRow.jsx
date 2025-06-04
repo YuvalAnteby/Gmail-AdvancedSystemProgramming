62b905cd10d899a222d28236f9e43809a3d1a891
yuval <yuvala2001@gmail.com>
2025-06-04 14:47:55 +0300

    GA-103 created a row component with some minimal styling

---
+++ frontend/src/main_page/components/MailRow.jsx
@@ -0,0 +1,44 @@
+import "./MailRow.css"
+
+
+/**
+ * props:
+ *   - id: {Number} unique identifier for an email
+ *   - sender: {String} name of the sender
+ *   - subject: {string}
+ *   - body: {string}
+ *   - date: {string} in format YYYY/MM/DD
+ *   - isSelected: {boolean} (whether this row is currently checked)
+ *   - onSelect: (id: string, checked: boolean) => void
+ */
+const MailRow = ({id, sender, subject, body, date, isSelected, onSelect}) => {
+    return (
+        <div className="row align-items-center mail-row py-2">
+            <div className="col-auto">
+                <input
+                    type="checkbox"
+                    checked={isSelected}
+                    onChange={(e) => onSelect(id, e.target.checked)}
+                />
+            </div>
+
+            <div className="col">
+                <strong>{sender}</strong>
+            </div>
+
+            <div className="col">
+                <p>{subject}</p>
+            </div>
+
+            <div className="col">
+                {body}
+            </div>
+
+            <div className="col text-end">
+                {date}
+            </div>
+        </div>
+    );
+}
+
+export default MailRow;
