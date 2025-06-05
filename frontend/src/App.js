8b06725c0efc362f1fd9d8c7adf5fdaf47086d0a
yuval <yuvala2001@gmail.com>
2025-06-05 18:00:08 +0300

    GA-103 Implemented dark theme a new light theme, moved colors palettes to theme.css for consistency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,12 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import MainPage from "./main_page/MainPage";
 
 function App() {
+    const theme = 'light'; ///TODO use a toggle button and update either here or within pages
 
   return (
     <div className="App">
         <Routes>
-            <Route path="/inbox" element={<MainPage/>} />
+            <Route path="/inbox" element={<MainPage theme={theme} />} />
         </Routes>
     </div>
   );
