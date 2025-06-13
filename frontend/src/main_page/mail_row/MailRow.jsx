c55f53f0849795575ff494378de57e000491352d
yuval <yuvala2001@gmail.com>
2025-06-13 23:28:49 +0300

    GA-103 UI change in mails rows

--- frontend/src/main_page/mail_row/MailRow.jsx
+++ frontend/src/main_page/mail_row/MailRow.jsx
@@ -1,49 +1,43 @@
 import "./MailRow.css"
+import {formatDate} from "../../utils/formatDate";
 
 
 /**
  * props:
- *   - theme: {String} dark or light according to user prefrence
- *   - id: {Number} unique identifier for an email
- *   - sender: {String} name of the sender
- *   - subject: {string}
- *   - body: {string}
- *   - date: {string} in format YYYY/MM/DD
- *   - isRead: {boolean} true if read the mail already
+ *   - theme: {String} dark or light according to user preference
+ *   - email: {Object} email object
  *   - isSelected: {boolean} (whether this row is currently checked)
  *   - onSelect: function when marking a mail as selected for mass actions on them
  */
-const MailRow = ({theme, id, sender, subject, body, date, isRead, isSelected, onSelect}) => {
+const MailRow = ({theme, email, isSelected, onSelect}) => {
 
     const handleMailOpen = () => {
-        console.log(">> Open Mail Row:", {id, sender, subject, body, date});
+        console.log(">> Open Mail Row:", email);
         /// TODO open the mail to read
     }
 
-    const rowClass = isRead ? "read" : "unread";
+    const rowClass = email.isRead ? "read" : "unread";
 
     return (
-        <div className={`row align-items-center ${theme}-mail-row ${rowClass} py-2`}
-             onClick={handleMailOpen}>
-            <div className="col-auto">
-                <input
-                    type="checkbox"
-                    checked={isSelected}
-                    onChange={(e) => onSelect(id, e.target.checked)}
-                    onClick={(e) => e.stopPropagation()} // <-- prevents row click
+        <div className={`mail-row-item ${theme} ${rowClass}`} onClick={handleMailOpen}>
+            <input
+                type="checkbox"
+                checked={isSelected}
+                onChange={(e) => onSelect(email.id, e.target.checked)}
+                onClick={(e) => e.stopPropagation()}
+            />
 
-                />
+            <div className="email-content">
+                <div className="email-sender">{email.from.fullName}</div>
+                <div className="email-main-line">
+                    <div className="email-title-body">
+                        <div className="email-subject">{email.subject}</div>
+                        <div className="email-preview">{email.body}</div>
+                    </div>
+                </div>
             </div>
+            <div className="email-time">{formatDate(email.sentAt || email.createdAt)}</div>
 
-            <div className="col">
-                <strong style={{color: `var(--${theme}-text-color)`}}>{sender.mail}</strong>
-            </div>
-
-            <div className="col" style={{color: `var(--${theme}-text-color)`}}>{subject}</div>
-
-            <div className="col" style={{color: `var(--${theme}-text-color)`}}>{body}</div>
-
-            <div className="col text-end" style={{color: `var(--${theme}-text-color)`}}>{date}</div>
         </div>
     );
 }
