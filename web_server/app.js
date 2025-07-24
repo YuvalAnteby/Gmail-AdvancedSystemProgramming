af7214dcce8afcf7dfa6d3cbbd16e4902b93f015
yuval <yuvala2001@gmail.com>
2025-07-24 20:28:03 +0300

    GA-136 removed console logs and todos

--- web_server/app.js
+++ web_server/app.js
@@ -8,8 +8,6 @@ const app = express()
 const PORT = process.env.NODE_PORT || 3001;
 const JSON_LIMIT = process.env.JSON_LIMIT || '10mb';
 const REACT_URL = process.env.REACT_URL || 'http://localhost:3000';
-/// todo remove
-console.log('port: ' + PORT, ', REACT_URL:' + REACT_URL + ', JSON_LIMIT: ' + JSON_LIMIT + ', JWT_SECRET:' + process.env.JWT_SECRET);
 
 
 app.use(express.json({limit: JSON_LIMIT}));
