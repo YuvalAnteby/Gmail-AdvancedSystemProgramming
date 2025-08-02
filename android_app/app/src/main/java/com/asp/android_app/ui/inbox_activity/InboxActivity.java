badc00a3d620175144e634e980db884c1aef4861
yuval <yuvala2001@gmail.com>
2025-08-02 17:56:38 +0300

    GA-139 creating new labels

--- android_app/app/src/main/java/com/asp/android_app/ui/inbox_activity/InboxActivity.java
+++ android_app/app/src/main/java/com/asp/android_app/ui/inbox_activity/InboxActivity.java
@@ -72,8 +72,6 @@ public class InboxActivity extends AppCompatActivity {
         // initialize user related observers and data sources
         UserViewModel userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         initializeUserObservers(userViewModel);
-
-
     }
 
     /**
@@ -158,6 +156,7 @@ public class InboxActivity extends AppCompatActivity {
 
         NavigationView navView = findViewById(R.id.navigation_view);
         navView.setNavigationItemSelectedListener(item -> {
+            clearAllMenuSelection();
             int id = item.getItemId();
             drawerLayout.closeDrawer(GravityCompat.START);
             item.setChecked(true);
@@ -188,6 +187,10 @@ public class InboxActivity extends AppCompatActivity {
             }
             if (id == R.id.nav_spam) {
                 inboxFragment.setInbox("spam");
+                return true;
+            }
+            if (id == R.id.nav_create_label) {
+                showCreateLabelDialog(labelViewModel);
                 return true;
             }
             return false;
@@ -239,11 +242,35 @@ public class InboxActivity extends AppCompatActivity {
      * @param labelViewModel view model containing the single source of truth
      */
     private void initializeLabels(LabelViewModel labelViewModel) {
+        // observer for fetching labels
         labelViewModel.getAllLabels().observe(this, result -> {
             if (result == null) {
                 return;
             }
             populateLabelsInDrawer(result);
+        });
+        // observer for label creation
+        labelViewModel.getCreatedLabel().observe(this, result -> {
+            if (result == null)
+                return;
+            labelViewModel.fetchAllLabels();
+        });
+        labelViewModel.getCreatedSublabel().observe(this, result -> {
+            if (result == null)
+                return;
+            labelViewModel.fetchAllLabels();
+        });
+        // observer for editing
+        labelViewModel.getLabelUpdated().observe(this, result -> {
+            if (result == null)
+                return;
+            labelViewModel.fetchAllLabels();
+        });
+        // observer for deletion
+        labelViewModel.getLabelDeleted().observe(this, result -> {
+            if (result == null)
+                return;
+            labelViewModel.fetchAllLabels();
         });
     }
 
@@ -275,7 +302,8 @@ public class InboxActivity extends AppCompatActivity {
             MenuItem item = menu.add(R.id.nav_dynamic_labels_group, Menu.NONE, baseOrder + i, label.getName());
             item.setIcon(R.drawable.ic_label);
             item.setOnMenuItemClickListener(menuItem -> {
-                // TODO set label clicks
+                clearAllMenuSelection();
+                menuItem.setChecked(true);
                 inboxFragment.setInbox("label:" + label.getId());
                 DrawerLayout drawerLayout = findViewById(R.id.main);
                 drawerLayout.closeDrawer(GravityCompat.START);
@@ -293,4 +321,93 @@ public class InboxActivity extends AppCompatActivity {
                     .setIcon(R.drawable.ic_add);
         }
     }
+
+    /**
+     * Unchecks all items in the drawer menu (static and dynamic).
+     */
+    private void clearAllMenuSelection() {
+        NavigationView navView = findViewById(R.id.navigation_view);
+        Menu menu = navView.getMenu();
+
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            item.setChecked(false);
+            // Handle nested groups like nav_dynamic_labels_group
+            if (item.hasSubMenu())
+                for (int j = 0; j < item.getSubMenu().size(); j++)
+                    item.getSubMenu().getItem(j).setChecked(false);
+        }
+    }
+
+
+    /**
+     * Shows a dialog to create a new label
+     *
+     * @param labelViewModel view model containing the single source of truth
+     */
+    private void showCreateLabelDialog(LabelViewModel labelViewModel) {
+        final EditText etName = new EditText(this);
+        etName.setHint(R.string.label_hint);
+        new MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.new_label)
+                .setView(etName)
+                .setPositiveButton(R.string.next, (dialog, which) -> {
+                    String labelName = etName.getText().toString().trim();
+                    if (labelName.isBlank()) {
+                        Toast.makeText(this, R.string.name_required_error, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    showSublabelChoiceDialog(labelViewModel, labelName);
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    /**
+     * Shows a dialog to create a new sub label
+     *
+     * @param labelViewModel view model containing the single source of truth
+     * @param labelName      new sub label's name
+     */
+    private void showSublabelChoiceDialog(LabelViewModel labelViewModel, String labelName) {
+        String[] options = getResources().getStringArray(R.array.label_options);
+        List<Label> currentLabels = labelViewModel.getAllLabels().getValue();
+        // if no labels exist yet - just create the label as a parent label
+        if (currentLabels == null || currentLabels.isEmpty()) {
+            labelViewModel.createLabel(labelName);
+            labelViewModel.getAllLabels();
+            return;
+        }
+        // let the user pick if it's a parent label or sub label
+        new MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.create_label_as)
+                .setItems(options, (dialog, which) -> {
+                    if (which == 0) {
+                        labelViewModel.createLabel(labelName);
+                        labelViewModel.getAllLabels();
+                    } else {
+                        // Open another dialog with list of existing labels
+                        showParentPickerDialog(labelViewModel, labelName, currentLabels);
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Shows a dialog to pick a parent label for a sub label
+     *
+     * @param labelVM view model containing the single source of truth
+     * @param name    new sub label's name
+     */
+    private void showParentPickerDialog(LabelViewModel labelVM, String name, List<Label> labels) {
+        String[] labelNames = labels.stream().map(Label::getName).toArray(String[]::new);
+        new MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.pick_parent_label)
+                .setItems(labelNames, (dialog, which) -> {
+                    int parentId = labels.get(which).getId();
+                    labelVM.createSublabel(parentId, name);
+                })
+                .show();
+    }
+
 }
